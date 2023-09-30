5e936aad4fe27cfa152e14f1986f7667e2352d8f
J0EK3R <J0EK3R@users.noreply.github.com>
2023-09-30 06:11:38 +0200

    JK: Added BluetoothLEAdvertisingDevice * added MouldKing Robot (MK_Robot) * added HOGOKIDS Robot (HK_Robot)

---
+++ BrickController2/BrickController2/PlatformServices/BluetoothLE/IBluetoothLEAdvertiserDevice .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BrickController2.PlatformServices.BluetoothLE
+{
+    public interface IBluetoothLEAdvertiserDevice :
+        IDisposable
+    {
+        Task<bool> StartAdvertiseAsync(int manufacturerId, byte[] rawData);
+
+        Task<bool> StopAdvertiseAsync();
+
+        bool ChangeAdvertiseAsync(int manufacturerId, byte[] rawData);
+    }
+}
