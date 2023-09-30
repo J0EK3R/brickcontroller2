ebc3579a1a356c81a7cb2b12d45148d762264721
J0EK3R <J0EK3R@users.noreply.github.com>
2023-09-30 06:11:38 +0200

    JK: Added BluetoothLEAdvertisingDevice * added MouldKing Robot (MK_Robot) * added HOGOKIDS Robot (HK_Robot)

---
+++ BrickController2/BrickController2.Android/PlatformServices/BluetoothLE/BluetoothLEAdvertiserDevice.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+using Android.Bluetooth;
+using Android.Bluetooth.LE;
+using BrickController2.PlatformServices.BluetoothLE;
+using Android.Runtime;
+
+namespace BrickController2.Droid.PlatformServices.BluetoothLE
+{
+    internal class BluetoothLEAdvertiserDevice : AdvertisingSetCallback,
+        IBluetoothLEAdvertiserDevice
+    {
+        private readonly BluetoothLeAdvertiser _advertiser;
+        private AdvertisingSet _advertisingSet;
+
+        public BluetoothLEAdvertiserDevice(BluetoothLeAdvertiser advertiser)
+        {
+            this._advertiser = advertiser;
+        }
+
+        public async Task<bool> StartAdvertiseAsync(int manufacturerId, byte[] rawData)
+        {
+            AdvertisingSetParameters settings = new AdvertisingSetParameters.Builder()
+                .SetLegacyMode(true)
+                .SetConnectable(true)
+                .SetScannable(true)
+                .SetInterval(AdvertisingSetParameters.IntervalMedium)
+                .SetTxPowerLevel(AdvertiseTxPower.Max)
+                .Build();
+
+            AdvertiseData data = new AdvertiseData.Builder()
+                .AddManufacturerData(manufacturerId, rawData)
+                .Build();
+
+            this._advertiser.StartAdvertisingSet(
+                settings,
+                data,
+                null,
+                null,
+                null,
+                this);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> StopAdvertiseAsync()
+        {
+            this._advertiser.StopAdvertisingSet(this);
+
+
+            return await Task.FromResult(true);
+        }
+
+        public bool ChangeAdvertiseAsync(int manufacturerId, byte[] rawData)
+        {
+            if(this._advertisingSet != null)
+            {
+                AdvertiseData data = new AdvertiseData.Builder()
+                    .AddManufacturerData(manufacturerId, rawData)
+                    .Build();
+
+                this._advertisingSet.SetAdvertisingData(data);
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override void OnAdvertisingSetStarted(AdvertisingSet advertisingSet, int txPower, [GeneratedEnum] AdvertiseResult status)
+        {
+            base.OnAdvertisingSetStarted(advertisingSet, txPower, status);
+
+            this._advertisingSet = advertisingSet;
+        }
+    }
+}
---
+++ BrickController2/BrickController2/DeviceManagement/BluetoothAdvertisingDevice.cs
@@ -0,0 +1,249 @@
+﻿using BrickController2.Helpers;
+using BrickController2.PlatformServices.BluetoothLE;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BrickController2.DeviceManagement
+{
+    internal abstract class BluetoothAdvertisingDevice<_TelegramType> : Device
+    {
+        #region Constants
+        protected static readonly Dictionary<_TelegramType, byte[]> Telegrams = new Dictionary<_TelegramType, byte[]>();
+        #endregion
+        #region Fields
+        protected readonly IBluetoothLEService _bleService;
+        protected readonly int _manufacturerId;
+        protected readonly object _outputLock = new object();
+
+        private Task _outputTask;
+        private CancellationTokenSource _outputTaskTokenSource;
+
+        private IBluetoothLEAdvertiserDevice _bleAdvertiserDevice;
+
+        private int counter = 0;
+
+        protected _TelegramType _currentTelegram;
+        #endregion
+
+        #region Constructor
+        protected BluetoothAdvertisingDevice(string name, string address, byte[] deviceData, IDeviceRepository deviceRepository, IBluetoothLEService bleService)
+            : base(name, address, deviceRepository)
+        {
+            this._bleService = bleService;
+
+            try
+            {
+                this._manufacturerId = BitConverter.ToInt32(deviceData);
+            }
+            catch
+            {
+                this._manufacturerId = 0x0666;
+            }
+        }
+        #endregion
+
+        #region InitFirstTelegram()
+        protected abstract _TelegramType InitFirstTelegram();
+        #endregion
+
+        #region ConnectAsync
+        public async override Task<DeviceConnectionResult> ConnectAsync(
+            bool reconnect,
+            Action<Device> onDeviceDisconnected,
+            IEnumerable<ChannelConfiguration> channelConfigurations,
+            bool startOutputProcessing,
+            bool requestDeviceInformation,
+            CancellationToken token)
+        {
+            using (await _asyncLock.LockAsync())
+            {
+                if (this._bleAdvertiserDevice != null ||
+                    this.DeviceState != DeviceState.Disconnected)
+                {
+                    HardwareVersion = "DeviceState isn't Disconnected";
+                    return DeviceConnectionResult.Error;
+                }
+
+                try
+                {
+                    // get advertiserdevice from BLEService
+                    this._bleAdvertiserDevice = this._bleService.GetBluetoothLEAdvertiserDevice();
+
+                    if (this._bleAdvertiserDevice == null)
+                    {
+                        HardwareVersion = "Can't get BluetoothLEAdvertiserDevice";
+                        return DeviceConnectionResult.Error;
+                    }
+
+                    HardwareVersion = "Connecting";
+                    this.DeviceState = DeviceState.Connecting;
+
+                    //token.ThrowIfCancellationRequested();
+
+                    if (startOutputProcessing)
+                    {
+                        _currentTelegram = InitFirstTelegram();
+                        await StartOutputTaskAsync();
+                    }
+
+                    token.ThrowIfCancellationRequested();
+
+                    HardwareVersion = $"Connected";
+
+                    this.DeviceState = DeviceState.Connected;
+                    return DeviceConnectionResult.Ok;
+                }
+                catch (OperationCanceledException)
+                {
+                    await DisconnectInternalAsync();
+
+                    HardwareVersion = "Connection Canceled";
+                    return DeviceConnectionResult.Canceled;
+                }
+                catch (Exception exception)
+                {
+                    await DisconnectInternalAsync();
+
+                    HardwareVersion = exception.Message;
+                    return DeviceConnectionResult.Error;
+                }
+            }
+        }
+        #endregion
+        #region DisconnectAsync
+        public override async Task DisconnectAsync()
+        {
+            using (await _asyncLock.LockAsync())
+            {
+                if (this.DeviceState == DeviceState.Disconnected)
+                {
+                    return;
+                }
+
+                await DisconnectInternalAsync();
+            }
+        }
+        #endregion
+
+        #region DisconnectInternalAsync
+        private async Task DisconnectInternalAsync()
+        {
+            if (this._bleAdvertiserDevice != null)
+            {
+                HardwareVersion = "Disconnecting";
+                this.DeviceState = DeviceState.Disconnecting;
+
+                await StopOutputTaskAsync();
+
+                this._bleAdvertiserDevice.Dispose();
+                this._bleAdvertiserDevice = null;
+            }
+
+            HardwareVersion = "Disconnected";
+            this.DeviceState = DeviceState.Disconnected;
+        }
+        #endregion
+
+        #region StartOutputTaskAsync
+        private async Task StartOutputTaskAsync()
+        {
+            await StopOutputTaskAsync();
+
+            this._outputTaskTokenSource = new CancellationTokenSource();
+            CancellationToken token = _outputTaskTokenSource.Token;
+
+            this._outputTask = Task.Run(async () =>
+            {
+                try
+                {
+                    byte[] currentData;
+                    if (Telegrams.TryGetValue(_currentTelegram, out currentData))
+                    {
+                        await this._bleAdvertiserDevice?.StartAdvertiseAsync(_manufacturerId, currentData);
+                    }
+
+                    await ProcessOutputsAsync(token).ConfigureAwait(false);
+
+                    FirmwareVersion = "Task Started";
+                }
+                catch (Exception exception)
+                {
+                    FirmwareVersion = $"StartOutputTaskAsync: {exception.Message}";
+                }
+            });
+        }
+        #endregion
+        #region StopOutputTaskAsync
+        private async Task StopOutputTaskAsync()
+        {
+            if (this._outputTaskTokenSource != null &&
+                this._outputTask != null)
+            {
+                this._outputTaskTokenSource.Cancel();
+
+                await this._outputTask;
+                
+                this._outputTaskTokenSource.Dispose();
+                this._outputTaskTokenSource = null;
+                
+                this._outputTask = null;
+            }
+
+            await this._bleAdvertiserDevice.StopAdvertiseAsync();
+            FirmwareVersion = "Task Stopped";
+        }
+        #endregion
+        #region ProcessOutputsAsync
+        protected async Task ProcessOutputsAsync(CancellationToken token)
+        {
+            try
+            {
+                bool isInitialized = false;
+                _TelegramType _lastSend = default(_TelegramType);
+
+                while (!token.IsCancellationRequested)
+                {
+                    bool valuesChanged = false;
+
+                    lock (_outputLock)
+                    {
+                        if (!isInitialized)
+                        {
+                            valuesChanged = true;
+                        }
+                        else if (!object.Equals(_lastSend, _currentTelegram))
+                        {
+                            valuesChanged = true;
+                        }
+                    }
+
+                    if (valuesChanged &&
+                        this._bleAdvertiserDevice != null)
+                    {
+                        byte[] currentData;
+                        if (Telegrams.TryGetValue(_currentTelegram, out currentData))
+                        {
+                            _lastSend = _currentTelegram;
+                            isInitialized = true;
+
+                            bool result = this._bleAdvertiserDevice.ChangeAdvertiseAsync(_manufacturerId, currentData);
+
+                            counter++;
+                            FirmwareVersion = $"{counter}";
+                        }
+                    }
+
+                    await Task.Delay(100, token).ConfigureAwait(false);
+                }
+            }
+            catch (Exception exception)
+            {
+                FirmwareVersion = $"ProcessOutputsAsync: {exception.Message}";
+            }
+        }
+        #endregion
+    }
+}
