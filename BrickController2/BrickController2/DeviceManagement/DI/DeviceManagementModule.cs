00cfc82386eb567ab5e3e03281cd2080d0c87b4c
J0EK3R <J0EK3R@users.noreply.github.com>
2023-10-01 07:38:11 +0200

    JK: renamed HK_Robot to HOGOKIDS_8051

--- BrickController2/BrickController2/DeviceManagement/DI/DeviceManagementModule.cs
+++ BrickController2/BrickController2/DeviceManagement/DI/DeviceManagementModule.cs
@@ -34,7 +34,7 @@ namespace BrickController2.DeviceManagement.DI
             builder.RegisterType<Cada_C51072W>().Keyed<Device>(DeviceType.Cada_C51072W);
 
             // Cada
-            builder.RegisterType<Cada_RC_C51072W>().Keyed<Device>(DeviceType.Cada_RC_C51072W);
+            builder.RegisterType<Cada_C51072W_RaceCar>().Keyed<Device>(DeviceType.Cada_C51072W_RaceCar);
 
             builder.Register<DeviceFactory>(c =>
             {
