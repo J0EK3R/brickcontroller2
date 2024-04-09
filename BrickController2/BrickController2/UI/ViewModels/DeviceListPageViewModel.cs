482f23f239de617e6fcc98191e90889eccc31043
J0EK3R <J0EK3R3.3@WIN11>
2024-04-09 08:41:24 +0200

    JK: added Button "DeleteAllDevices"

--- BrickController2/BrickController2/UI/ViewModels/DeviceListPageViewModel.cs
+++ BrickController2/BrickController2/UI/ViewModels/DeviceListPageViewModel.cs
@@ -8,6 +8,7 @@ using Device = BrickController2.DeviceManagement.Device;
 using BrickController2.UI.Commands;
 using System.Threading;
 using BrickController2.UI.Services.Translation;
+using Xamarin.Forms;
 
 namespace BrickController2.UI.ViewModels
 {
@@ -29,6 +30,7 @@ namespace BrickController2.UI.ViewModels
             _dialogService = dialogService;
 
             ScanCommand = new SafeCommand(async () => await ScanAsync(), () => !DeviceManager.IsScanning);
+            DeleteAllDevicesCommand = new SafeCommand(async () => await DeleteAllDevicesAsync(), () => !DeviceManager.IsScanning);
             DeviceTappedCommand = new SafeCommand<Device>(async device => await NavigationService.NavigateToAsync<DevicePageViewModel>(new NavigationParameters(("device", device))));
             DeleteDeviceCommand = new SafeCommand<Device>(async device => await DeleteDeviceAsync(device));
         }
@@ -36,6 +38,7 @@ namespace BrickController2.UI.ViewModels
         public IDeviceManager DeviceManager { get; }
 
         public ICommand ScanCommand { get; }
+        public ICommand DeleteAllDevicesCommand { get; }
         public ICommand DeviceTappedCommand { get; }
         public ICommand DeleteDeviceCommand { get; }
 
@@ -131,6 +134,28 @@ namespace BrickController2.UI.ViewModels
                     Translate("ErrorDuringScanning"),
                     Translate("Ok"),
                     CancellationToken.None);
+            }
+        }
+
+        private async Task DeleteAllDevicesAsync()
+        {
+            try
+            {
+                if (await _dialogService.ShowQuestionDialogAsync(
+                    Translate("Confirm"),
+                    $"{Translate("AreYouSureToDeleteDevice")} 'All'?",
+                    Translate("Yes"),
+                    Translate("No"),
+                    _disappearingTokenSource.Token))
+                {
+                    await _dialogService.ShowProgressDialogAsync(
+                        false,
+                        async (progressDialog, token) => await DeviceManager.DeleteDevicesAsync(),
+                        Translate("Deleting"));
+                }
+            }
+            catch (OperationCanceledException)
+            {
             }
         }
     }
