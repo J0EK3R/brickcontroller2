112e6b2314891e6e005851f26934a9c9a9af5d05
J0EK3R <J0EK3R3.3@WIN11>
2024-12-09 06:09:09 +0100

    JK: backward compatibility - ControllerEvents without ControllerDeviceId always match ControllerDeviceId-check

--- BrickController2/BrickController2/BusinessLogic/PlayLogic.cs
+++ BrickController2/BrickController2/BusinessLogic/PlayLogic.cs
@@ -80,7 +80,8 @@ namespace BrickController2.BusinessLogic
             {
                 foreach (var controllerEvent in ActiveProfile.ControllerEvents)
                 {
-                    if (e.ControllerDeviceId == controllerEvent.ControllerDeviceId &&
+                    if ((string.IsNullOrEmpty(controllerEvent.ControllerDeviceId) ||      // for backward compatibility with undef ControllerDeviceId
+                        e.ControllerDeviceId == controllerEvent.ControllerDeviceId) &&
                         gameControllerEvent.Key.EventType == controllerEvent.EventType &&
                         gameControllerEvent.Key.EventCode == controllerEvent.EventCode)
                     {
